4c9f6f0cbd548111071f071d1269d185d4b7b859
Michal Dobšínský <dog560154@gmail.com>
2022-11-10 08:54:39 +0100

    uwu

---
+++ Ruwu.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+library(dplyr)
+mydata <- readRDS("./M03_2021.RDS")
+print(mydata)
+print(ncol(mydata)) #get number of colums
+length (mydata)
+count(mydata[])
+print(sko)
+uwu <- 0;
+for (skola in mydata) {
+  uwu+1
+}
+print(uwu)
